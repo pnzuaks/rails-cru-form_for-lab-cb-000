b5a85945d690c584595bd0fe7b6ff849e6060cb8
Patricia Nykaza <pnykaza@calstatela.edu>
2018-11-07 19:05:16 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,7 +12,6 @@ class SongsController < ApplicationController
   def create
     @song = Song.new(song_params(:name, :artist_id, :genre_id))
     @song.save
-
     redirect_to song_path(@song)
   end
 
